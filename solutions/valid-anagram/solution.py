597492840ce71a0a882660584d83ffd7fc28204b
juna311 <109890582+juna311@users.noreply.github.com>
2026-07-14 11:11:06 +0900

    Solve Valid Anagram (LeetCode 2066760037)
    Imported from an accepted LeetCode submission.
    Original submission: 2026-07-14T02:11:06+00:00
    Author date preserves submission time; committer date records import time.

---
+++ solutions/valid-anagram/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        firstString = {}
+        for i in range(0, len(s)):
+            if s[i] in firstString:
+                firstString[s[i]] = firstString[s[i]] + 1
+            else:
+                firstString[s[i]] = 1
+        for j in range(0, len(t)):
+            if t[j] in firstString:
+                firstString[t[j]] = firstString[t[j]] - 1
+            else:
+                firstString[t[j]] = -1
+        for k in firstString.values():
+            if k != 0:
+                return False
+        return True
+
