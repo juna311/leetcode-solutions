321628435bc60ffabf475bc22d8fbd7c4109cb99
juna311 <109890582+juna311@users.noreply.github.com>
2026-09-15 10:44:58 +0900

    Delete arrays-and-hashing directory

--- arrays-and-hashing/contains-duplicate/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        checked = set()
-        for i in range(0, len(nums)):
-            if nums[i] in checked:
-                return True
-            else:
-                checked.add(nums[i])
-        return False
